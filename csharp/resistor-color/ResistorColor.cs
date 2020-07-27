2cae5cb757a21e751ec63d5e85224971bf614365
colormerad <amandamaydoodle@gmail.com>
2020-07-27 10:31:54 -0400

    New exercises

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,24 +2,14 @@
 
 public static class ResistorColor
 {
+    readonly static string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
     public static int ColorCode(string color)
     {
-        int colorVal = 0;
-        string[] colors = Colors();
-        for (int i = 0; i < colors.Length; i++)
-        {
-            if (colors[i] == color)
-            {
-                colorVal = i;
-                break;
-            }
-        }
-        return colorVal;
+        return Array.IndexOf(colors, color);
     }
-
     public static string[] Colors()
     {
-        string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
         return colors;
     }
 }
---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+
+        var result = new List<int>();
+        foreach (var num in multiples.ToList())
+        {
+            if (num != 0)
+            {
+                var numbers = Enumerable.Range(num, max)
+                            .Where(x => x % num == 0)
+                            .Where(x => x < max)
+                            .ToList();
+                result.AddRange(numbers);
+            }          
+        }
+        return result.Distinct().Sum();        
+    }
+}
